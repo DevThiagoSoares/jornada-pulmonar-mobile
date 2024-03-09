c427fd48c581738fed74089d13c18bcb40c30cee
fernanda.souza <fernanda.souza@grupoicts.com.br>
2024-03-09 01:27:08 -0400

    feat/card

---
+++ src/screens/Question/components/list-questions.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { View } from 'react-native';
+import { Button, List, TextInput } from 'react-native-paper';
+
+import FormComponent from './form-question';
+import { styledForm } from './styles';
+
+export function ListQuestions() {
+  const [questions, setQuestions] = useState([{ id: 1 }]);
+  const addQuestion = () => {
+    const newId = questions.length + 1;
+    setQuestions([...questions, { id: newId }]);
+  };
+  return (
+    <View style={{ display: 'flex', padding: 10, gap: 20 }}>
+      <TextInput label="Título da Unidade" mode="outlined" outlineColor="transparent" />
+      <List.AccordionGroup>
+        {questions.map((question) => (
+          <List.Accordion title={`Questão ${question.id}`} id={question.id} key={question.id}>
+            <FormComponent />
+          </List.Accordion>
+        ))}
+      </List.AccordionGroup>
+      <Button onPress={addQuestion} mode="contained" style={styledForm.button}>
+        Criar Nova Questão
+      </Button>
+    </View>
+  );
+}
