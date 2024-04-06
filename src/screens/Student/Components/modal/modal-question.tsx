ce1d9f8df84dd5e64f89d4773ff97e0e44ac0b43
fernanda.souza <fernanda.souza@grupoicts.com.br>
2024-04-06 01:17:04 -0400

    fix: alterTable

--- src/screens/Student/Components/modal/modal-question.tsx
+++ src/screens/Student/Components/modal/modal-question.tsx
@@ -1,5 +1,6 @@
+import { Ionicons } from '@expo/vector-icons';
 import { useState } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import { View } from 'react-native-animatable';
 import { ScrollView } from 'react-native-gesture-handler';
 import imgHome from 'src/assets/image/Grupo-6845.png';
@@ -18,6 +19,11 @@ import { styles } from '~/screens/Login/styles';
 export function ModalQuestion() {
   const [locationX, setLocationX] = useState<number>(0);
   const [locationY, setLocationY] = useState<number>(0);
+  const [isActive, setActive] = useState(false);
+  const defaultPosition = {
+    latX: 142.4761962890625,
+    lgnY: 155.38987731933594,
+  };
 
   const options = [
     { value: 'a', description: 'test' },
@@ -27,8 +33,14 @@ export function ModalQuestion() {
   ];
   const handlePress = (event: { nativeEvent: { locationX: any; locationY: any } }) => {
     const { locationX, locationY } = event.nativeEvent;
+    console.log({ locationX, locationY });
     setLocationX(locationX - 170);
     setLocationY(locationY);
+  };
+  const handlePosition = () => {
+    /*  setLocationX(defaultPosition.latX - 150);
+    setLocationY(defaultPosition.lgnY - 50); */
+    setActive(!isActive);
   };
   return (
     <BackgroundScreen source={imgHome} style={styles.backgroundImage} resizeMode="cover">
@@ -38,6 +50,18 @@ export function ModalQuestion() {
           <Text style={styledSelect.title}>Clique no paciente para fazer a ausculta pulmonar</Text>
         </View>
         <ScrollView horizontal showsHorizontalScrollIndicator={false} onTouchStart={handlePress}>
+          <TouchableOpacity
+            onPress={handlePosition}
+            style={[
+              styledModal.iconImg,
+              { left: defaultPosition.latX, top: defaultPosition.lgnY },
+            ]}>
+            {defaultPosition && isActive ? (
+              <Ionicons name="volume-high-outline" size={20} color="#CD4C3E" />
+            ) : (
+              <Ionicons name="volume-mute-outline" size={20} color="#CD4C3E" />
+            )}
+          </TouchableOpacity>
           <View style={styledModal.carouselContainer}>
             <CarouselComponent titleImg="Titulo da imagem" img={img} />
             <CarouselComponent titleImg="Titulo da imagem" img={img} />
@@ -47,7 +71,7 @@ export function ModalQuestion() {
         <View>
           <AlternativaCard options={options} />
         </View>
-        <SelectPosition newLocationX={0} newLocationY={0} />
+        <SelectPosition newLocationX={locationX} newLocationY={locationY} />
       </ScrollView>
     </BackgroundScreen>
   );
--- src/screens/Student/Components/modal/style.tsx
+++ src/screens/Student/Components/modal/style.tsx
@@ -14,4 +14,11 @@ export const styledModal = StyleSheet.create({
     gap: 30,
     margin: 20,
   },
+  iconImg: {
+    position: 'absolute',
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    borderRadius: 20,
+    padding: 10,
+    zIndex: 2,
+  },
 });
