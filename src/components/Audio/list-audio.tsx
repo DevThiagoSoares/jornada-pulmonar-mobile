11abaa04b94d394a734c02fa7594a1386b0bbf81
fernanda.souza <fernanda.souza@grupoicts.com.br>
2024-04-26 15:04:22 -0400

    feat:update property

--- src/components/Audio/list-audio.tsx
+++ src/components/Audio/list-audio.tsx
@@ -33,7 +33,7 @@ export function ListAudio(props: audioProps) {
 
   const audioFiles = Array.from({ length: totalAudios }, (_, index) => ({
     id: index + 1,
-    title: `Caso ${index + 1}`,
+    title: `Caso - ${index + 1}`,
     uri: getAudioUri(index + 1),
   }));
 
