0255c5e173bc651f26cad30e888a46d4e2e7f82a
fernanda.souza <fernanda.souza@grupoicts.com.br>
2024-07-09 15:17:45 -0400

    feat/loading-button

--- src/screens/Student/Components/componentImg/index.tsx
+++ src/screens/Student/Components/componentImg/index.tsx
@@ -3,6 +3,7 @@ import { Audio } from 'expo-av';
 import React, { useEffect, useRef, useState } from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import Toast from 'react-native-toast-message';
+import { getUrlFile } from 'utils/downloadFile';
 
 import { defaultPosition } from './positions';
 import { styles } from './styles';
@@ -25,10 +26,15 @@ interface Coordinate {
 export function CarouselComponent(props: ImgProps) {
   const [isActive, setActive] = useState(false);
   const [audio, setAudio] = useState<any>();
+  const [updateListAudios, setUpdateListAudios] = useState<any>([]);
   const { question } = useQuestion();
   const [getIndex, setIndex] = useState<number | null>(null);
   const soundObject = useRef(new Audio.Sound()).current;
   const arrayListAudios = question.audioUrl.length > 0 ? JSON.parse(question.audioUrl) : [];
+
+  useEffect(() => {
+    handleListAudios();
+  }, []);
 
   useEffect(() => {
     handleStartAudio();
@@ -37,6 +43,19 @@ export function CarouselComponent(props: ImgProps) {
   const handlePosition = () => {
     Toast.hide();
     setActive(true);
+  };
+
+  const handleListAudios = async () => {
+    if (arrayListAudios.length > 0) {
+      const updatedArrayListAudios = await Promise.all(
+        arrayListAudios.map(async (item: any) => {
+          const refFile = item.audioUrl.split('/').pop();
+          const newAudioUrl = await getUrlFile(refFile);
+          return { audioUrl: newAudioUrl };
+        })
+      );
+      setUpdateListAudios(updatedArrayListAudios);
+    }
   };
 
   const handlefindPosition = (index: number) => {
@@ -49,26 +68,33 @@ export function CarouselComponent(props: ImgProps) {
 
   const handleStartAudio = async () => {
     if (audio) {
-      if (soundObject) {
-        try {
-          await soundObject.unloadAsync();
-          await soundObject.loadAsync({ uri: audio });
-          await soundObject.playAsync();
-        } catch (error) {
-          console.error('Erro ao carregar/reproduzir áudio:', error);
+      try {
+        if (soundObject) {
+          // Verifique se o áudio está carregado antes de tentar parar e descarregar
+          const status = await soundObject.getStatusAsync();
+          if (status.isLoaded) {
+            await soundObject.stopAsync();
+            await soundObject.unloadAsync();
+          }
         }
+
+        // Carregue e inicie o novo áudio
+        await soundObject.loadAsync({ uri: audio }, { shouldPlay: true });
+        await soundObject.setPositionAsync(0);
+        await soundObject.playAsync();
+      } catch (error) {
+        console.error('Erro ao carregar/reproduzir áudio:', error);
       }
     }
   };
-
   const handleAudioIconPress = async (index: number) => {
     setActive(true);
     handlePosition();
     handlefindPosition(index);
-    if (arrayListAudios.length > 3) {
-      setAudio(arrayListAudios[props.idImg - 1].audioUrl);
+    if (updateListAudios.length > 3) {
+      setAudio(updateListAudios[props.idImg - 1].audioUrl);
     } else {
-      setAudio(arrayListAudios[0].audioUrl);
+      setAudio(updateListAudios[0].audioUrl);
     }
   };
 
